fe9db8d0710d981e541f68b93b814163752169c2
kimhyeji <gpwl5589@gmail.com>
2025-10-15 16:47:45 +0900

    init data

--- src/main/java/com/rest/proj/domain/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/rest/proj/domain/article/controller/ApiV1ArticleController.java
@@ -1,30 +1,31 @@
 package com.rest.proj.domain.article.controller;
 
 import com.rest.proj.domain.article.entity.Article;
+import com.rest.proj.domain.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/articles")
 public class ApiV1ArticleController {
+    private final ArticleService articleService;
 
     @GetMapping
     public List<Article> getArticles() {
-        List<Article> articles = new ArrayList<>();
+        List<Article> articles = articleService.getList();
 
         return articles;
     }
 
     @GetMapping("/{id}")
     public Article getArticle(@PathVariable("id") Long id) {
-        Article article = new Article();
+        Article article = articleService.getArticle(id);
 
         return article;
     }
--- src/main/java/com/rest/proj/domain/article/service/ArticleService.java
+++ src/main/java/com/rest/proj/domain/article/service/ArticleService.java
@@ -1,7 +1,38 @@
 package com.rest.proj.domain.article.service;
 
+import com.rest.proj.domain.article.entity.Article;
+import com.rest.proj.domain.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public void create(String subject, String content) {
+        Article article = Article.builder()
+                .subject(subject)
+                .content(content)
+                .build();
+
+        articleRepository.save(article);
+    }
+
+    public List<Article> getList() {
+        return articleRepository.findAll();
+    }
+
+    public Article getArticle(Long id) {
+        Optional<Article> oq = articleRepository.findById(id);
+
+        if ( oq.isEmpty() ) {
+            return null;
+        }
+
+        return oq.get();
+    }
 }
