2436229a345d7daca134c3b4b0adbceb9def3e43
kimhyeji <gpwl5589@gmail.com>
2025-10-20 18:23:15 +0900

    프론트 화면에서 등록

--- frontapp/app/article/page.tsx
+++ frontapp/app/article/page.tsx
@@ -1,24 +1,26 @@
 "use client";
 
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Article() {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    getData();
+    fetchArticles();
   }, []);
 
-  const getData = async () => {
-    const result = await fetch("http://localhost:8090/api/v1/articles").then(
-      (row) => row.json()
-    );
-    setArticles(result.data.articles);
-    console.log(result.data.articles);
+  const fetchArticles = () => {
+    fetch("http://localhost:8090/api/v1/articles")
+      .then((result) => result.json())
+      .then((result) => setArticles(result.data.articles))
+      .catch((err) => console.error(err));
   };
 
   return (
     <>
+      <ArticleForm fetchArticles={fetchArticles} />
+
       <h4>번호 / 제목 / 생성일</h4>
       {articles.length == 0 ? (
         <p>현재 게시물이 없습니다.</p>
@@ -26,11 +28,72 @@ export default function Article() {
         <ul>
           {articles.map((article) => (
             <li key={article.id}>
-              {article.id}/{article.subject} / {article.createdDate}
+              {article.id} /
+              <Link href={`/article/${article.id}`}>{article.subject}</Link> /
+              {article.createdDate}
             </li>
           ))}
         </ul>
       )}
+    </>
+  );
+}
+
+function ArticleForm({ fetchArticles }) {
+  const [article, setArticle] = useState({ subject: "", content: "" });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:8090/api/v1/articles", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(article),
+    });
+
+    if (response.ok) {
+      alert("success");
+      fetchArticles();
+      setArticle({ subject: "", content: "" });
+    } else {
+      alert("fail");
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setArticle({ ...article, [name]: value });
+    // console.log({ ...article, [name]: value });
+  };
+
+  return (
+    <>
+      <h4>게시물 작성</h4>
+      <form onSubmit={handleSubmit}>
+        <label>
+          제목 :
+          <input
+            type="text"
+            name="subject"
+            onChange={handleChange}
+            value={article.subject}
+          />
+        </label>
+        <br />
+        <label>
+          내용 :
+          <input
+            type="text"
+            name="content"
+            onChange={handleChange}
+            value={article.content}
+          />
+        </label>
+        <input type="submit" value="등록" />
+        {/* <button type="submit">등록</button> */}
+      </form>
     </>
   );
 }
