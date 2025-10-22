935537d3c56b75a446bd5b4cc9e8be631b4ec3ed
kimhyeji <gpwl5589@gmail.com>
2025-10-22 15:27:12 +0900

    jwt filter 경로 수정

--- src/main/java/com/rest/proj/global/security/JwtAuthorizationFilter.java
+++ src/main/java/com/rest/proj/global/security/JwtAuthorizationFilter.java
@@ -22,7 +22,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     @Override
     @SneakyThrows
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
-        if (request.getRequestURI().contains("/api/v1/login") || request.getRequestURI().equals("/api/v1/members/logout")) {
+        if (request.getRequestURI().contains("/api/v1/members/login") || request.getRequestURI().equals("/api/v1/members/logout")) {
             filterChain.doFilter(request, response);
             return;
         }
