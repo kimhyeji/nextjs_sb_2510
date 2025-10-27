4560eeef81fa019ed69e6e25c6dde6b9c278277d
kimhyeji <gpwl5589@gmail.com>
2025-10-27 15:23:19 +0900

    article detail useQuery

--- frontapp/src/app/article/[id]/page.tsx
+++ frontapp/src/app/article/[id]/page.tsx
@@ -1,29 +1,36 @@
 'use client'
 
 import { useParams } from 'next/navigation'
-import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import api from '@/src/utils/api'
+import { useQuery } from '@tanstack/react-query'
 
 export default function ArticleDetail() {
   const params = useParams()
-  const [article, setArticle] = useState({})
 
-  useEffect(() => {
-    api
+  const getArticle = async () => {
+    return await api
       .get(`/articles/${params.id}`)
-      .then((response) => setArticle(response.data.data.article))
-      .catch((err) => console.log(err))
-  }, [])
+      .then((res) => res.data.data.article)
+  }
 
-  return (
-    <>
-      <h4>게시물 상세 {params.id}번</h4>
-      <div>{article.subject}</div>
-      <div>{article.content}</div>
-      <div>{article.createdDate}</div>
-      <div>{article.modifiedDate}</div>
-      <Link href={`/article/${params.id}/edit`}>수정</Link>
-    </>
-  )
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['article'],
+    queryFn: getArticle,
+  })
+
+  if (error) console.log(error)
+
+  if (isLoading) <>Loading....</>
+
+  if (data) {
+    return (
+      <>
+        <h4>게시물 상세 {params.id}번</h4>
+        <div>{data.subject}</div>
+        <div>{data.author}</div>
+        <Link href={`/article/${params.id}/edit`}>수정</Link>
+      </>
+    )
+  }
 }
